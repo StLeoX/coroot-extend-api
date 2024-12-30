78114c33e24beeb70e2f3526ba1339521e39bc91
StLeoX <stleox@qq.com>
2024-12-30 10:42:20 +0800

    Change grpc version.

--- examples/main.go
+++ examples/main.go
@@ -3,9 +3,12 @@ package main
 import (
 	"fmt"
 	eventv1 "github.com/StLeoX/coroot-extend-api/api/proto/coroot/event/v1"
+	"google.golang.org/grpc"
 )
 
 func main() {
 	span := &eventv1.ServerSpan{}
 	fmt.Println(span)
+
+	fmt.Println(grpc.SupportPackageIsVersion9)
 }
